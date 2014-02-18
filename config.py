62a3954a0d5b5f6ac44019d3f4ddd969eef39c40
mekpro <mekpro@gmail.com>
2014-02-18 22:13:43 +0700

    server

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 MONGO_SERVER = 'localhost'
 MONGO_DB = 'coinstalk'
+WEB_PORT = 8092
 
 URLS = [
   ['mtgox', 'http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast'],
@@ -7,3 +8,18 @@ URLS = [
   ['btc-e', 'https://btc-e.com/api/2/btc_usd/ticker'],
   ['bitfinex', 'https://api.bitfinex.com/v1/ticker/btcusd'],
 ]
+
+TIME_RANGE = {
+  '3hr': 60 * 60 * 3,
+  '24hr': 60 * 60 * 24,
+  '7d' : 60 * 60 * 24 * 7,
+  '30d': 60 * 60 * 24 * 30,
+}
+
+SAMPLING_RATE = {
+  '3hr' : 180,
+  '24hr' : 288,
+  '7d' : 336,
+  '30d' : 336,
+}
+
--- server.py
+++ server.py
@@ -6,14 +6,39 @@ from pymongo import MongoClient
 
 import config
 
+def sampling(rows, count, rate):
+  values = []
+  step_size = count / rate
+  c = 0
+  for row in rows:
+    if c >= 0:
+      c += step_size
+      values.append(row['last'])
+    c -= 1
+  return values
+
+def query_values(trader, time_range_str, time_end):
+  values = []
+  conn = MongoClient(config.MONGO_SERVER)[config.MONGO_DB]
+  sampling_rate = config.SAMPLING_RATE[time_range_str]
+  time_range = config.TIME_RANGE[time_range_str]
+  time_start = time_end - datetime.timedelta(seconds=time_range)
+  query = {
+    'trader': trader,
+    'timestamp' : { '$gt' : time_start, '$lte': time_end},
+  }
+  rows = conn.value.find(query)
+  count = rows.count()
+  values = sampling(rows, count, sampling_rate)
+  return values
 
 @get('/')
 def view_root():
   return {'hello': 'coinstalk'}
 
-@get('/api/query/<trader>/<time>')
-def api_query():
-
+@get('/api/values/<trader>/<timerange>')
+def api_values():
+  return {'haha' : 'olo'}
 
 
 if __name__ == '__main__':
