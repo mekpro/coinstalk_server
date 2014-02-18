d85147a6f7330cf641e3e038eba4cbdceac848ab
mekpro <mekpro@gmail.com>
2014-02-18 11:24:30 +0700

    bitcoin pricerate crawler

---
+++ config.py
@@ -0,0 +1,9 @@
+MONGO_SERVER = 'localhost'
+MONGO_DB = 'coinstalk'
+
+URLS = [
+  ['mtgox', 'http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast'],
+  ['bitstamp', 'https://www.bitstamp.net/api/ticker/'],
+  ['btc-e', 'https://btc-e.com/api/2/btc_usd/ticker'],
+  ['bitfinex', 'https://api.bitfinex.com/v1/ticker/btcusd'],
+]
---
+++ crawler.py
@@ -0,0 +1,57 @@
+import eventlet
+import logging
+import json
+from pymongo import MongoClient
+from eventlet.green import urllib2
+
+import config
+
+def fetch(url):
+  content = urllib2.urlopen(url[1]).read()
+  return (url[0], content)
+
+def record(parser, content):
+  conn = MongoClient(config.MONGO_SERVER)[config.MONGO_DB]
+  d = json.loads(content)
+  result = dict()
+  if parser == 'mtgox':
+    result['trader'] = 'mtgox'
+    result['timestamp'] = int(d['data']['now'])
+    last_s = d['data']['last_local']['display'].split('$')[1]
+    result['last'] = float(last_s)
+    conn.value.insert(result)
+  elif parser == 'bitstamp':
+    result['trader'] = 'bitstamp'
+    result['timestamp'] = int(d['timestamp'])
+    result['last'] = float(d['last'])
+    result['high'] = float(d['high'])
+    result['bid'] = float(d['bid'])
+    result['volume'] = float(d['volume'])
+    result['low'] = float(d['low'])
+    result['ask'] = float(d['ask'])
+    conn.value.insert(result)
+  elif parser == 'btc-e':
+    result['trader'] = 'btc-e'
+    d = d['ticker']
+    result['timestamp'] = int(d['updated'])
+    result['last'] = float(d['last'])
+    result['high'] = float(d['high'])
+    result['volume'] = float(d['vol_cur'])
+    result['low'] = float(d['low'])
+    conn.value.insert(result)
+  elif parser == 'bitfinex':
+    result['trader'] = 'bitfinex'
+    result['timestamp'] = round(float(d['timestamp']))
+    result['last'] = float(d['last_price'])
+    result['mid'] = float(d['mid'])
+    result['ask'] = float(d['ask'])
+    conn.value.insert(result)
+    pass
+  else:
+    logging.error('invalid parser: '+ parser)
+
+if __name__ == '__main__':
+  pool = eventlet.GreenPool()
+  for body in pool.imap(fetch, config.URLS):
+    record(body[0], body[1])
+
